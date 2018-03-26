73c76cc9d4e225b5ca61a0925f0f897c93573b98
Matthew Buchanan <matt@matthews-air.home>
2018-03-26 11:27:09 -0400

    more questions, new game button, images, styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,31 +4,44 @@ $(document).ready(function() {
     var trivia = [
         {question: 'This bird is popular in cooking, but eating it can cause an illness known as "coturnism" which is the result of the bird feeding on hemlock.', 
         options: ['Goose', 'Duck', 'Turkey', 'Quail'],
-        answer: 'Quail'}, 
+        answer: 'Quail',
+        image: 'assets/images/quail.jpg'}, 
 
         {question: 'This bird is also known as the "butcherbird" due its habit of impaling insects and small mammals on thorns or barbed wire, to eat later.',
         options: ['Nighthawk', 'Shrike', 'Kite', 'Phoebe'],
-        answer: 'Shrike'},
+        answer: 'Shrike',
+        image: 'assets/images/shrike.jpg'},
+
+        {question: 'The kea is a large species of parrot. The kea is highly curious about human materials such as backpacks and boots, and is even known for prying apart the rubber parts of cars. It is a native of this island.',
+        options: ['New Zealand', 'Galapagos', 'Phillipines', 'Madagascar'],
+        answer: 'New Zealand',
+        image: 'assets/images/kea.jpg'},
 
         {question: 'What is the airspeed velocity of an un-laden swallow?', 
         options: ['60 km/hr', '42', '1,192 km/hr', 'What do you mean, an African or European swallow?'],
-        answer: 'What do you mean, an African or European swallow?'},
+        answer: 'What do you mean, an African or European swallow?',
+        image: 'assets/images/montypython.jpg'},
     ]
 
-    var imageLinks = [];
-
-    var triviaIndex = 0;
+    var triviaIndex;
 
     var timerID;
-    var timerCount= 30;
-    
-    $('.game').prepend('<button class="start">Start Game</button>');
+    var timerCount;
+    var questionsCorrect;
 
-    $('.start').click(function() {
-        $('.game').html('<p class="timer"></p>');
-        $('.game').append('<div class="board"></div>');
-        startClock();
-        renderTrivia();
+    $('body').on('click', '.start', function() {
+        console.log('Game started.')
+        triviaIndex = 0;
+        timerCount = 30;
+        questionsCorrect = 0;
+        setTimeout(function() {
+            $('.game').empty();
+            $('.game').html('<p class="timer"></p>');
+            $('.game').append('<div class="board"></div>');
+            startClock();
+            renderTrivia();
+        }, 500);
+        
     });
 
     function startClock() {
@@ -47,14 +60,26 @@ $(document).ready(function() {
 
     $('body').on('click', '.clickable', function() {
         $('.board').html(`Your answer was ${$(this).text()}. The correct answer is ${trivia[triviaIndex].answer}`);
+        $('.board').append(`<div><img src="${trivia[triviaIndex].image}"></div>`);
         clearInterval(timerID);
         setTimeout(function() {
             startClock();
             triviaIndex++;
-            renderTrivia();
-        }, 5000);
+            if (!gameIsComplete()) {
+                renderTrivia();
+            }
+        }, 1000);
         
     });
+
+    function gameIsComplete() {
+        if ((timerCount <= 0) || (triviaIndex === trivia.length)) {
+            $('.board').html(`Game Over. You got ${questionsCorrect} questions right out of ${trivia.length} questions.`);
+            clearInterval(timerID);
+            $('.board').append('<div><button class="start">New Game</button></div>');
+            return true;
+        } else {return false;}
+    }
 
 
 });
